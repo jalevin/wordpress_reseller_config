ad967e815afc1a145ea0d2e4eee8166b7eb4af81
Jeff Levin <jeff@freezerburned.org>
2016-02-08 22:31:14 -0900

    wp-cli user install

---
+++ wp-cli-install.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+#install wp-cli
+
+mkdir -p ~/bin
+cwd /bin
+wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+
+#add to .bashrc to load at login
+sed -i 'source "$HOME/bin/wp-completion.bash"' ~/.bashrc
+sed -i 'export PATH="$HOME/bin:$PATH"' ~/.bashrc
+
+source ~/.bashrc
