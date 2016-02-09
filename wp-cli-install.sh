58d255f90dc05bc0b6916f79d2adf8cf6d05677b
Jeff Levin <jeff@freezerburned.org>
2016-02-08 22:37:57 -0900

    host doesn't support sed
    rework
    
    use -O for wget
    
    make name file for wget
    
    name files in wget command
    
    added some comments
    
    no need for double quotes

--- wp-cli-install.sh
+++ wp-cli-install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #install wp-cli
 
-mkdir -p ~/bin
-cwd /bin
-wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
+mkdir -p ~/bin;
+wget  -O ~/bin/wp-cli.phar https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar;
+wget  -O ~/bin/wp-completion.bash https://github.com/wp-cli/wp-cli/raw/master/utils/wp-completion.bash
+chmod +x ~/bin/wp-cli.phar
 
 #add to .bashrc to load at login
-sed -i 'source "$HOME/bin/wp-completion.bash"' ~/.bashrc
-sed -i 'export PATH="$HOME/bin:$PATH"' ~/.bashrc
-
+echo '#add wp-cli to path and autocomplete"' | cat - ~/.bashrc > temp && mv temp ~/.bashrc 
+echo 'source ~/bin/wp-completion.bash' | cat - ~/.bashrc > temp && mv temp ~/.bashrc 
+echo 'export PATH="$HOME/bin:$PATH"'     | cat - ~/.bashrc > temp && mv temp  ~/.bashrc
 source ~/.bashrc
